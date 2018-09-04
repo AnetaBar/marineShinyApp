82d5674759f353cf5186768292f5440dd81c4b25
AnetaBar <anetaabar@gmail.com>
2018-09-04 21:48:49 +0200

    Calculate distance, show appropriate message and display results on a map

--- app.R
+++ app.R
@@ -1,13 +1,15 @@
 library(shiny)
 library(leaflet)
 library(dplyr)
-ships <- read.csv(file = "ships.csv")
+library(geosphere)
+library(data.table)
+ships <- read.csv(file = "ships.csv", stringsAsFactors=FALSE)
 
 
 # Define UI ----
 ui <- fluidPage(
   titlePanel(h2("Marine Shiny App")),
-
+  
   sidebarLayout(
     sidebarPanel(
       h3("Please select a vessel:"),
@@ -15,9 +17,8 @@ ui <- fluidPage(
       uiOutput("vesselNameInput")
     ),
     mainPanel(
-      h3("A map"),
-      leafletOutput("mymap",height = 500),
-      p("The longest distance")
+      leafletOutput("map",height = 500),
+      textOutput("longestDistanceOutput")
     )
   )
 )
@@ -30,15 +31,33 @@ server <- function(input, output, session) {
   })
   
   output$vesselNameInput <- renderUI({
-    selectInput("vesselName", "Name of vessel:", unique(subset(ships, ship_type == input$vesselType, select = c(SHIPNAME))))
+    selectInput("vesselName", "Name of vessel:", unique(unlist(subset(ships, ship_type == input$vesselType, select = c(SHIPNAME)))))
   })
   
-  output$mymap <- renderLeaflet({
-    m <- leaflet() %>%
+  output$map <- renderLeaflet({
+    maximumDistanceRow <- calculateMaximumDistance(input$vesselName)
+    m <- leaflet(maximumDistanceRow) %>%
       addTiles() %>%
-      setView(lng=18.580730, lat=54.387248 , zoom=10)
+      addMarkers(lng = c(maximumDistanceRow$LON, maximumDistanceRow$LON_TO), lat=c(maximumDistanceRow$LAT, maximumDistanceRow$LAT_TO))
     m
   })
+  
+  output$longestDistanceOutput <- renderText({
+    maximumDistanceRow <- calculateMaximumDistance(input$vesselName)
+    paste("The maximum distance covered between two consecutive observations is ", round(maximumDistanceRow$DISTANCE, 2), "m.")
+  })
+  
+  calculateMaximumDistance <- function(vesselName) {
+    singleShipDf <- subset(ships, SHIPNAME == vesselName, select = c(LON, LAT, DATETIME, SHIP_ID))
+    setDT(singleShipDf)
+    singleShipDf[, `:=`(LON_TO = shift(LON, type = "lead"), LAT_TO = shift(LAT, type = "lead"))]
+    singleShipDf[, `:=`(DISTANCE = distVincentyEllipsoid(singleShipDf[,1:2], singleShipDf[,5:6]))]
+    maxDistanceRow <- singleShipDf[singleShipDf$DISTANCE == max(singleShipDf$DISTANCE, na.rm = TRUE),]
+    if (length(maxDistanceRow) > 1) {
+      maxDistanceRow <- maxDistanceRow[maxDistanceRow$DATETIME == max(maxDistanceRow$DATETIME, na.rm = TRUE),]
+    }
+    return (maxDistanceRow)
+  }
 }
 
 # Run the app ----
